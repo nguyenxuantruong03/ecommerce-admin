955e86f6cd1c4115d097e2790f1c7fa1f28cb9a5
asdiop159752 <nxt159753@gmail.com>
2023-07-06 09:03:24 +0700

    Setting page (Admin)

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -9,6 +9,9 @@ import { useEffect } from "react";
   const isOpen =useStoreModal((state) => state.isOpen);
 
   useEffect(()=>{
+    /* Mã này đang kiểm tra xem biến `isOpen` có phải là `false` hay không (có nghĩa là phương thức hiện không
+     mở). Nếu nó là `false`, thì nó gọi hàm `onOpen`, hàm này có lẽ sẽ mở phương thức. Cái này
+     đảm bảo rằng phương thức luôn mở khi thành phần `SetupPage` được hiển thị. */
     if(!isOpen){
       onOpen()
     }
--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { AlertModal } from "@/components/modals/alert-modal"
 import { StoreModal } from "@/components/modals/sotre-modal"
 
 import {useEffect,useState} from "react"
