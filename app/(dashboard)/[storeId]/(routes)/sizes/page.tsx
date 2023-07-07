7bcf6368c174bbea597d71b5b1048237b352d1b1
asdiop159752 <nxt159753@gmail.com>
2023-07-07 11:15:18 +0700

    Size Entity (Admin)

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,31 @@
+import prismadb from "@/lib/prismadb";
+import SizeClient from "./components/client";
+import { SizeColumn } from "./components/columns";
+import { format } from "date-fns";
+
+const SizePage = async ({params}:{params:{storeId:string}}) => {
+    const sizes = await prismadb.size.findMany({
+        where:{
+            storeId:params.storeId
+        },
+        orderBy:{
+            createdAt: 'desc'
+        }
+    });
+
+    const fomattedSizes: SizeColumn[] = sizes.map((item)=>({
+        id: item.id,
+        name: item.name,
+        value: item.value,
+        createdAt:format(item.createdAt, 'MMMM do, yyyy')
+    }))
+    return ( 
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <SizeClient data={fomattedSizes} />
+            </div>
+        </div>
+     );
+}
+ 
+export default SizePage;
