40a0774b225f3b60cd3d356a8a5f03c96ddf4304
asdiop159752 <nxt159753@gmail.com>
2023-07-09 16:18:03 +0700

    Dashboard page (Admin)

---
+++ actions/get-total-revenue.ts
@@ -0,0 +1,24 @@
+import prismadb from "@/lib/prismadb"
+
+export const getTotalrevenue = async (storeId: string)=>{
+    const paidOrder= await prismadb.order.findMany({
+        where:{
+            storeId,
+            isPaid: true
+        },
+        include:{
+            orderItems:{
+                include:{
+                    product: true
+                }
+            }
+        }
+    })
+    const totalRevenue = paidOrder.reduce((total,order)=>{
+        const orderTotal= order.orderItems.reduce((orderSum,item)=>{
+            return orderSum + item.product.price.toNumber()
+        },0);
+        return total + orderTotal
+    },0)
+    return totalRevenue
+}
