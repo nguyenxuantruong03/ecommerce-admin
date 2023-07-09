9e3025a3027b1e88198d7d58a3a596b8a14b35ac
asdiop159752 <nxt159753@gmail.com>
2023-07-09 16:26:36 +0700

    Dark mode(Admin)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToasterProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme.provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,9 +23,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <ModalProvider />
           <ToasterProvider />
           {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import StoreSwitcher from "@/components/store-switcher";
 
 import {redirect} from "next/navigation"
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "./theme-toggle";
 
 const Navbar = async () => {
     const {userId} = auth();
@@ -24,6 +25,7 @@ const Navbar = async () => {
                 <StoreSwitcher items={store}/>
                <MainNav className="mx-6"/>
                 <div className="ml-auto flex items-center space-x-4">
+                    <ThemeToggle />
                     <UserButton  afterSignOutUrl="/"/>
                 </div>
             </div>
